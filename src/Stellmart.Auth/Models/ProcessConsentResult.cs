72e603c96c7537561cac24a7c7cbb1c966de3f90
Amel Salibasic <amel@fourthzone.co.uk>
2018-04-29 23:40:32 +0100

    auth solution

---
+++ src/Stellmart.Auth/Models/ProcessConsentResult.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Stellmart.Auth.ViewModels;
+
+namespace Stellmart.Auth.Models
+{
+    public class ProcessConsentResult
+    {
+        public bool IsRedirect => RedirectUri != null;
+        public string RedirectUri { get; set; }
+        public string ClientId { get; set; }
+
+        public bool ShowView => ViewModel != null;
+        public ConsentViewModel ViewModel { get; set; }
+
+        public bool HasValidationError => ValidationError != null;
+        public string ValidationError { get; set; }
+    }
+}
